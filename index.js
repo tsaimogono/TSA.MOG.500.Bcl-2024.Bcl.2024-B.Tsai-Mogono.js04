54ae514ac5a679cbccdaf79a4b67ce0eb955c200
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-03-19 15:04:26 +0200

    change commit

--- index.js
+++ index.js
@@ -2,13 +2,9 @@
 
 // #### Challenge 1: Flavor Selection
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
-// print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
+// print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular"
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
-
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-
 // Using a ternary operator to check user preference and print the appropriate message
 console.log(`User prefers ${userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular'} variety.`);
 
@@ -17,10 +13,6 @@ console.log(`User prefers ${userPreference === 'Sugar-free' ? 'Sugar-free' : 'Re
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
-
-let cansLeft = 3; // Any number of cans
-
 // Using a ternary operator to check the number of cans left and print the appropriate message
 console.log(cansLeft < 5 ? 'Time to restock!' : 'We\'re stocked!');
 
@@ -31,15 +23,11 @@ console.log(cansLeft < 5 ? 'Time to restock!' : 'We\'re stocked!');
 let heartRate = 95; // Current heart rate in bpm
 
 console.log(heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!');
-console.log(`'}`);
-
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
-
 // Using a ternary operator to check the current temperature and print the appropriate message
 console.log(currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!');
 
